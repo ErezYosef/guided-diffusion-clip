62c1e9d7c52e4ed209e8d7f43ca81810a220272b
ErezYosef <Erez.Yo@gmail.com>
2021-12-25 19:17:00 +0200

    adding SRImageModel_Feat (diff model with sr input image)

--- guided_diffusion/unet_other.py
+++ guided_diffusion/unet_other.py
@@ -39,3 +39,33 @@ class UNetModel_clip_feat(UNetModel):
         y = clip_feat.squeeze().float() # Nx512
         #print('generating y', y.shape)
         return super().forward(x, timesteps, y=y, **kwargs)
+
+class SRImageModel_Feat(UNetModel):
+    def __init__(self, image_size, in_channels, *args, **kwargs):
+        super().__init__(image_size, in_channels*2, *args, **kwargs)
+        if self.num_classes is not None:
+            self.label_emb = nn.Sequential(
+            linear(self.num_classes, self.time_embed_dim),
+            nn.SiLU(),
+            linear(self.time_embed_dim, self.time_embed_dim),
+        )
+        self.bias_feat = nn.Parameter(th.randn(self.num_classes))
+        print('layer_changed !', self.num_classes, self.time_embed_dim)
+
+    def forward(self, x, timesteps, clip_feat=None, clip_feat2=None, img2=None, **kwargs):
+
+        y = clip_feat.squeeze().float()  # Nx512
+        clip_feat2 = clip_feat2.squeeze().float()  # Nx512
+        #print('yshape', clip_feat2.shape, self.bias_feat)
+        y = y - clip_feat2 + self.bias_feat
+        #print('yshape', y.shape)
+        # print('generating y', y.shape)
+        B, C, new_height, new_width = x.shape
+        #upsampled = F.interpolate(low_res, (new_height, new_width), mode="bilinear")
+        #spat_feat_repaeted = clip_feat.float().view(B, 512).repeat(1, 32)  # hard coded
+        #spat_feat_repaeted_sized = spat_feat_repaeted.view(B, 1, 128,128)
+
+        #x = th.cat([x, spat_feat_repaeted_sized, th.transpose(spat_feat_repaeted_sized, 2, 3)], dim=1)
+        x = th.cat([x, img2], dim=1)
+        #print('xshape', x.shape)
+        return super().forward(x, timesteps, y=y, **kwargs)
